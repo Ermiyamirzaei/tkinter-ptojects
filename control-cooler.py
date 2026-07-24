62ca7df95a99762a688452fbcc6bec7aa5a02be3
Ermiya <ermiyamirzaei@gmail.com>
2026-07-24 15:42:00 +0330

    tempup,tempdown tamoom

--- control-cooler.py
+++ control-cooler.py
@@ -1,44 +1,90 @@
 from tkinter import *
-def click(valiu):
-    ...
+
+is_on = True
+
+def temp_up():
+    global is_on
+
+    if not is_on:
+        return
+
+    temp = int(entry.get().replace("°C", ""))
+
+    if temp < 30:
+        temp += 1
+
+    entry.delete(0, END)
+    entry.insert(0, f"{temp}°C")
+
+
+def temp_down():
+    global is_on
+
+    if not is_on:
+        return
+
+    temp = int(entry.get().replace("°C", ""))
+
+    if temp > 16:
+        temp -= 1
+
+    entry.delete(0, END)
+    entry.insert(0, f"{temp}°C")
+
 
 def shut_down():
-    ...
+    global is_on
+    is_on = False
+    entry.delete(0, END)
+    entry.insert(0, "OFF")
 
-def cooler():
-    ...
-    
+
 def turn_on():
-    ...
-
-
-
-
-
+    global is_on
+    is_on = True
+    entry.delete(0, END)
+    entry.insert(0, "24°C")
 
 
 root = Tk()
-root.title("cooler control")
-entry = Entry(root,width=15,font=("Arial", 22), justify=CENTER)
-entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
-entry.insert(0,"24°C")
+root.title("Cooler Control")
 root.geometry("250x300")
 
+entry = Entry(root, width=15, font=("Arial", 22), justify=CENTER)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+entry.insert(0, "24°C")
+
 buttons = [
-    ("↑",1,0), ("↓",1,3),("turn on",8,3)
+    ("↑", 1, 0),
+    ("↓", 1, 3),
+    ("turn on", 8, 3)
 ]
 
-
 for text, row, col in buttons:
-    if text== "turn on":
-        btn = Button(root, text=text, width=5, height=2, font=("Arial", 14), command=turn_on)
-    else:
-        btn = Button(root, text=text, width=5, height=2, font=("Arial", 14), command=lambda t=text: click(t))
-    btn.grid(row=row, column=col, padx=3,pady=5)
+
+    if text == "↑":
+        btn = Button(root, text=text,bg="#5ad941", width=5, height=2,
+                     font=("Arial", 14), command=temp_up)
+
+    elif text == "↓":
+        btn = Button(root, text=text, width=5,bg="#bdd941", height=2,
+                     font=("Arial", 14), command=temp_down)
+
+    elif text == "turn on":
+        btn = Button(root, text=text,bg="#4b41d9" ,width=8, height=2,
+                     font=("Arial", 14), command=turn_on)
+
+    btn.grid(row=row, column=col, padx=5, pady=5)
 
 
-shutdown = Button(root, text="shut down", width=8, height=2, font=("Arial", 14), command=shut_down)
+shutdown = Button(root,
+                  text="shut down",
+                  bg="#d94141",
+                  width=8,
+                  height=2,
+                  font=("Arial", 14),
+                  command=shut_down)
+
 shutdown.grid(row=8, column=0, columnspan=2, padx=5, pady=5)
-
 
 root.mainloop()

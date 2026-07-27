1c9a316bf3225f3d644907e89e86cf24676832dc
Ermiya <ermiyamirzaei@gmail.com>
2026-07-27 21:27:14 +0330

    initialize

---
+++ RPS.py
@@ -0,0 +1,19 @@
+import random
+from tkinter import *
+from tkinter import messagebox
+
+choice = ["rock","paper","scissors"]
+user_score = 0
+coputer_score = 0
+round_number = 1
+max_round = 5
+user_history = []
+
+def ai_chioce():
+    ...
+
+def check_winner(user, computer):
+    ...
+
+class RPSGame:
+    ...

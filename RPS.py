b3fd56027a1ec8b2a911aca8a6a1c969d0853834
Ermiya <ermiyamirzaei@gmail.com>
2026-07-30 15:58:25 +0330

    RPS desighed

--- RPS.py
+++ RPS.py
@@ -1,7 +1,6 @@
 import random
 from tkinter import *
 from tkinter import messagebox
-
 choices = ["rock","paper","scissors"]
 user_score = 0
 computer_score = 0
@@ -36,30 +35,31 @@ class RPSGame:
     def __init__(self, root):
         self.root = root
         self.root.title("rock paper scissers")
-
-        self.title = Label(root, text="rock paper scissers", font=("Arial", 18))
+        
+        self.title = Label(root, text="rock paper scissers",bg="Black", font=("Arial", 18, "italic"))
         self.title.pack(pady=10)
 
         self.info = Label(root, text="choose your move: ", font=("Arial", 18))
         self.title.pack(pady=10)
         #buttons
-        self.btn_rock = Button(root, text="🗿", command=lambda:self.play("rock"))
+        self.btn_rock = Button(root, text="rock🗿",font=("Apple Color Emoji", 20),bg="green", command=lambda:self.play("rock"))
         self.btn_rock.pack(pady=5)
 
-        self.btn_paper = Button(root, text="📃", command=lambda:self.play("paper"))
+        self.btn_paper = Button(root, text="paper📃",font=("Arial",20,"italic"),bg="indigo", command=lambda:self.play("paper"))
         self.btn_paper.pack(pady=5)
 
-        self.btn_scissors = Button(root, text="✂️", command=lambda:self.play("scissors"))
+        self.btn_scissors = Button(root, text="scissors✂",font=("Arial", 18,"bold"),bg="gray",width=8, command=lambda:self.play("scissors"))
         self.btn_scissors.pack(pady=5)
 
         self.result_lable = Label(root, text="")
         self.result_lable.pack(pady=5)
 
-        self.score_lable = Label(root, text="score = 0 - 0")
+        self.score_lable = Label(root, text="score = 0 -- 0",font=("Arial",14,"bold"),bg="#1ABC9C")
         self.score_lable.pack(pady=5)
 
         self.animate()
 
+        
     def animate(self):
         color = self.title.cget("fg")
         new_color = "red" if color == "black" else "black"
@@ -83,9 +83,9 @@ class RPSGame:
 
         else:
             computer_score += 1
-            self.result_lable.config(text=f"Computer Win!!")
+            self.result_lable.config(text=f"Computer Win!!",bg="red",font=("Arial",14,"bold"))
 
-        self.score_lable.config(text=f"Score: 🙍‍♂️ {user_score} - {computer_score}🐱‍🚀")
+        self.score_lable.config(text=f"Score:{user_score} - {computer_score}",font=("Calibri"),bg="#1ABC9C")
         round_number += 1
 
         if round_number > max_round:
@@ -105,4 +105,5 @@ class RPSGame:
         self.root.destroy()
 root = Tk()
 app = RPSGame(root)
+root.configure(bg="yellow")
 root.mainloop()
